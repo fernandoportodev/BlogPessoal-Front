b1a06ea43f09f823f6dfa1d67278230a66b125bf
Fernando Porto <fernandogomes143@gmail.com>
2022-04-11 01:59:47 -0300

    @fernandoportodev Task: Criando componente Tema PUT e Delete com rotas internas

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -21,7 +21,19 @@ export class TemaService {
     return this.http.get<Tema[]>('https://blogpessoalthiago.herokuapp.com/temas', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema> {
+    return this.http.get<Tema>(`blogpessoalthiago.herokuapp.com/temas/${id}`, this.token)
+  }
+
   postTema(tema : Tema): Observable<Tema>{
     return this.http.post<Tema>('https://blogpessoalthiago.herokuapp.com/temas', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://blogpessoalthiago.herokuapp.com/temas', tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`blogpessoalthiago.herokuapp.com/temas/${id}`, this.token)
   }
 }
